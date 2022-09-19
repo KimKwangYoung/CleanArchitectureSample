e5bfe22493c83804417e2d1fb05d1aa5b3ef0487
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-09-20 00:23:48 +0900

    remote 모듈 변경

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -1,7 +1,50 @@
 plugins {
-    id ("kotlin")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        buildConfigField("String", "NAVER_CLIENT_ID", "\"naver_client_id\"")
+        buildConfigField("String", "NAVER_SECRET_KEY", "\"naver_secret_key\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
 
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+
+    implementation ("com.google.dagger:hilt-android:$hiltVersion")
+    kapt ("com.google.dagger:hilt-android-compiler:$hiltVersion")
 }
--- remote/src/main/java/com/kky/data/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.kky.data
-
-class MyClass {
-}
