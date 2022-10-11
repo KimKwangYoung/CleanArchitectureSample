e55d232782994a558a00949d18581dbf74ceabd4
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-10-12 00:41:35 +0900

    BaseFragment 구현

---
+++ app/src/main/java/com/kky/cleanarchitecturesample/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.kky.cleanarchitecturesample.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<VB: ViewDataBinding, VM: ViewModel>: Fragment() {
+    private var _binding: VB? = null
+
+    protected val binding: VB get() = _binding ?: throw IllegalStateException("binding is not initialized")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bind()
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    abstract fun bind(): VB
+}
