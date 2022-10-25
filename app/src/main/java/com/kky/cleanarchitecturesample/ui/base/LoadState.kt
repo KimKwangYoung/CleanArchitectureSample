28c615a367bbe128438d1b425abc9cc8c7f99dfb
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-10-25 22:36:16 +0900

    UiState 요소에서 LoadState 추가

---
+++ app/src/main/java/com/kky/cleanarchitecturesample/ui/base/LoadState.kt
@@ -0,0 +1,7 @@
+package com.kky.cleanarchitecturesample.ui.base
+
+enum class LoadState {
+    LOADING,
+    SUCCESS,
+    ERROR
+}
---
+++ app/src/main/java/com/kky/cleanarchitecturesample/ui/base/UiState.kt
@@ -0,0 +1,3 @@
+package com.kky.cleanarchitecturesample.ui.base
+
+interface UiState
